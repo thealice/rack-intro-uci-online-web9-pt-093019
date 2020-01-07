154e0631adfc270a1c83c727f52a1bc7d865fa14
Alice Freda <alicefreda@gmail.com>
2020-01-07 19:58:07 +0000

    Automatically backed up by Learn

--- my_server.rb
+++ my_server.rb
@@ -2,8 +2,8 @@ class MyServer
   def call(env)
     return [ 200, {'Content-Type' => 'text/html'}, pretty_response ]
   end
- 
+
   def pretty_response
-    (Time.now.to_i % 2).zero? ?  ["<em>Hello, my name is</em>"] : ["<strong>Hello, my name is</strong>"]
+    (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
   end
 end
